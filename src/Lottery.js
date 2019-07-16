026589a6cc25d10601d078ce1535853e4dde1686
bsmith20681 <bsmith20681@gmail.com>
2019-07-15 18:04:42 -0600

    second commit

---
+++ src/Lottery.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Lottery extends Component {
+  static defaultProps = {
+    title: 'Lotto',
+    maxBalls: 6,
+    manNum: 40
+  }
+  constructor(props){
+    super(props);
+    this.state = {nums: Array.from({ length: this.props.maxBalls}) };
+  }
+  generate(){
+
+  }
+  handleClick(){
+    this.generate();
+  }
+
+  render(){
+    return(
+      <section className="Lottery">
+        <h1>{this.props.title}</h1>
+        <div className="">
+          Balls go here
+        </div>
+        <button onClick={this.handleClick}>Generate</button>
+      </section>
+    )
+  }
+
+}
+
+export default Lottery;
